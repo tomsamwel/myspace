10d203b4e474214e652829422085881e7d16dcc8
tomsamwel <tommiesamwel@gmail.com>
2019-09-11 16:41:05 +0200

    initial commit with db

---
+++ database/migrations/2019_09_11_133812_create_likes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+			$table->unsignedBigInteger('user_id');
+			$table->unsignedBigInteger('likeable_id');
+            $table->timestamps();
+			$table->softDeletes();	
+
+			$table->primary(['user_id', 'likeable_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
